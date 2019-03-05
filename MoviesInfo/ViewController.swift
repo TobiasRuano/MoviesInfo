2143b510e8aadf1998e3fad3fdd394060eac37c5
Tobias <ruano.t10@gmail.com>
2019-03-05 16:48:00 -0300

    Initial Commit

---
+++ MoviesInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesInfo
+//
+//  Created by Tobias Ruano on 05/03/2019.
+//  Copyright © 2019 Tobias Ruano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
