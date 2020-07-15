2f6e75c3a94df716665f336f620bbcd940bb451c
Tobias Ruano <ruano.t10@gmail.com>
2020-07-15 18:04:15 -0300

    added case to MIError file

--- MoviesInfo/Models/MIError.swift
+++ MoviesInfo/Models/MIError.swift
@@ -13,4 +13,5 @@ enum MIError: String, Error {
     case unableToComplete = "Unable to complete your request. Plese check your internet connection"
     case invalidResponse = "Invalid response from the server. Please try again"
     case invalidData = "The data recieved from the server was invalid."
+    case unableToParseData = "The data given does not match the Model."
 }
