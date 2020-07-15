a2d860861bb35621d668fd100c222238c1a2f305
Tobias Ruano <ruano.t10@gmail.com>
2020-07-15 11:00:49 -0300

    Plaheholder image is set if no image is downloaded

--- MoviesInfo/Views/ImageViews/MIImageView.swift
+++ MoviesInfo/Views/ImageViews/MIImageView.swift
@@ -29,7 +29,9 @@ class MIImageView: UIImageView {
         NetworkManager.shared.downloadImage(from: url) { [weak self] image in
             guard let self = self else { return }
             DispatchQueue.main.async {
-                self.image = image
+                if let imageToAdd = image {
+                    self.image = imageToAdd
+                }
             }
         }
     }
