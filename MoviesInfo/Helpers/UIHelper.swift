4f5f46607e53b1dda474c7f430a2c03d94c2917c
Tobias Ruano <ruano.t10@gmail.com>
2020-07-18 10:52:11 -0300

    Fixed collectionView layout on iPad for the search screen

--- MoviesInfo/Helpers/UIHelper.swift
+++ MoviesInfo/Helpers/UIHelper.swift
@@ -29,7 +29,7 @@ enum UIHelper {
         
         let flowLayout = UICollectionViewFlowLayout()
         flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth * 1.5)
         
         return flowLayout
     }
