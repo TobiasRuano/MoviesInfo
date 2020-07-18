cdd75c37a75fb0286270e2bf3d8ec2c65bcf231a
Tobias Ruano <ruano.t10@gmail.com>
2020-07-18 00:01:50 -0300

    Refactored SimilarMoviesCollectionView to its own childViewController

--- MoviesInfo/Controllers/MovieInfoViewController.swift
+++ MoviesInfo/Controllers/MovieInfoViewController.swift
@@ -10,19 +10,12 @@ import UIKit
 
 class MovieInfoViewController: UIViewController {
     
-    enum Section {
-        case main
-    }
-    
-    var collectionView: UICollectionView!
-    var dataSource: UICollectionViewDiffableDataSource<Section, Movie>!
+    var similarMoviesView: UIView!
     var movieView: MovieCardView!
     var scrollView: UIScrollView!
     var contenView: UIView!
     
-    var isLoadingMovies = false
     var movie: Movie!
-    var relatedMovies: [Movie] = []
     let network = NetworkManager.shared
     let imageCache = NSCache<AnyObject, AnyObject>()
     
@@ -32,9 +25,7 @@ class MovieInfoViewController: UIViewController {
         configureScrollView()
         configureContenView()
         configureMovieView()
-        configureCollectionView()
-        configureDataSource()
-        requestSimilarMovies(page: 1)
+        configureSimilarMoviesView()
     }
     
     func configureStyle() {
@@ -93,77 +84,26 @@ class MovieInfoViewController: UIViewController {
         ])
     }
     
-    func configureCollectionView()  {
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: UIHelper.createOneLineFlowLayout(in: view))
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.backgroundColor = .systemBackground
-        collectionView.showsHorizontalScrollIndicator = false
-        collectionView.delegate = self
-        collectionView.register(SimilarMovieCell.self, forCellWithReuseIdentifier: SimilarMovieCell.reuseID)
-        contenView.addSubview(collectionView)
+    func configureSimilarMoviesView()  {
+        similarMoviesView = UIView()
+        similarMoviesView.translatesAutoresizingMaskIntoConstraints = false
+        contenView.addSubview(similarMoviesView)
         
+        self.add(childVC: ImageCollectionViewController(movie: movie), to: self.similarMoviesView)
+
         NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: movieView.bottomAnchor, constant: 20),
-            collectionView.heightAnchor.constraint(equalToConstant: 200),
-            collectionView.bottomAnchor.constraint(equalTo: contenView.bottomAnchor),
-            collectionView.widthAnchor.constraint(equalTo: contenView.widthAnchor),
-            collectionView.centerXAnchor.constraint(equalTo: contenView.centerXAnchor)
+            similarMoviesView.topAnchor.constraint(equalTo: movieView.bottomAnchor, constant: 20),
+            similarMoviesView.heightAnchor.constraint(equalToConstant: 200),
+            similarMoviesView.bottomAnchor.constraint(equalTo: contenView.bottomAnchor),
+            similarMoviesView.widthAnchor.constraint(equalTo: contenView.widthAnchor),
+            similarMoviesView.centerXAnchor.constraint(equalTo: contenView.centerXAnchor)
         ])
     }
     
-    func configureDataSource() {
-        dataSource = UICollectionViewDiffableDataSource<Section, Movie>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, movie) -> UICollectionViewCell? in
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SimilarMovieCell.reuseID, for: indexPath) as! SimilarMovieCell
-            cell.setCell(with: movie)
-            return cell
-        })
-    }
-    
-    func requestSimilarMovies(page: Int) {
-        isLoadingMovies = true
-        let urltype = "movie/\(movie.id)/similar?"
-        let requestURL = network.getMovieURL(type: urltype, page: page)
-        network.fetchMovies(type: requestURL) { [weak self] result in
-            guard let self = self else { return }
-            switch result {
-            case .success(let movies):
-                self.updateUI(with: movies)
-            case .failure(let error):
-                print(error)
-            }
-            self.isLoadingMovies = false
+    private func add(childVC: UIViewController, to containerView: UIView) {
+            addChild(childVC)
+            containerView.addSubview(childVC.view)
+            childVC.view.frame = containerView.bounds
+            childVC.didMove(toParent: self)
         }
-    }
-    
-    func updateUI(with movies: [Movie]) {
-        relatedMovies.append(contentsOf: movies)
-        DispatchQueue.main.async {
-            if self.relatedMovies.isEmpty {
-                let emptyUIView = MIEmptyStateView(message: "Unable to find titles related to this movie.")
-                self.scrollView.addSubview(emptyUIView)
-                emptyUIView.frame = self.collectionView.frame
-            } else {
-                self.updateData(on: self.relatedMovies)
-            }
-        }
-    }
-    
-    func updateData(on followers: [Movie]) {
-        var snapshot = NSDiffableDataSourceSnapshot<Section, Movie>()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(followers)
-        DispatchQueue.main.async {
-            self.dataSource.apply(snapshot, animatingDifferences: true)
-        }
-    }
-}
-
-extension MovieInfoViewController: UICollectionViewDelegate {
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let destVC = MovieInfoViewController()
-        destVC.movie = relatedMovies[indexPath.row]
-        let navController = UINavigationController(rootViewController: destVC)
-        present(navController, animated: true)
-    }
 }
---
+++ MoviesInfo/Views/Controllers/ImageCollectionViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ImageCollectionViewController.swift
+//  MoviesInfo
+//
+//  Created by Tobias Ruano on 17/07/2020.
+//  Copyright © 2020 Tobias Ruano. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewController: UIViewController {
+    
+    enum Section {
+        case main
+    }
+    
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Movie>!
+    
+    var movie: Movie!
+    var relatedMovies: [Movie] = []
+    let network = NetworkManager.shared
+    
+    init(movie: Movie) {
+        super.init(nibName: nil, bundle: nil)
+        self.movie = movie
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureCollectionView()
+        configureDataSource()
+        requestSimilarMovies(page: 1)
+    }
+    
+    func configureCollectionView()  {
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: UIHelper.createOneLineFlowLayout(in: view))
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .systemBackground
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.delegate = self
+        collectionView.register(SimilarMovieCell.self, forCellWithReuseIdentifier: SimilarMovieCell.reuseID)
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Movie>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, movie) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SimilarMovieCell.reuseID, for: indexPath) as! SimilarMovieCell
+            cell.setCell(with: movie)
+            return cell
+        })
+    }
+    
+    func requestSimilarMovies(page: Int) {
+        let urltype = "movie/\(movie.id)/similar?"
+        let requestURL = network.getMovieURL(type: urltype, page: page)
+        network.fetchMovies(type: requestURL) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let movies):
+                self.updateUI(with: movies)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func updateUI(with movies: [Movie]) {
+        relatedMovies.append(contentsOf: movies)
+        DispatchQueue.main.async {
+            if self.relatedMovies.isEmpty {
+                let emptyUIView = MIEmptyStateView(message: "Unable to find titles related to this movie.")
+                self.view.addSubview(emptyUIView)
+                emptyUIView.frame = self.collectionView.frame
+            } else {
+                self.updateData(on: self.relatedMovies)
+            }
+        }
+    }
+    
+    func updateData(on followers: [Movie]) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Movie>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.main.async {
+            self.dataSource.apply(snapshot, animatingDifferences: true)
+        }
+    }
+}
+
+extension ImageCollectionViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let destVC = MovieInfoViewController()
+        destVC.movie = relatedMovies[indexPath.row]
+        let navController = UINavigationController(rootViewController: destVC)
+        present(navController, animated: true)
+    }
+}
