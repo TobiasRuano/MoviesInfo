cff976733bd84a51e05e7797dcc0fcf352f5e164
Tobias Ruano <ruano.t10@gmail.com>
2020-07-28 13:49:56 -0300

    Changed date format

--- MoviesInfo/Helpers/Date+Ext.swift
+++ MoviesInfo/Helpers/Date+Ext.swift
@@ -11,7 +11,7 @@ import Foundation
 extension Date {
     func convertToDayMonthYearFormat() -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd MMM yyyy"
+        dateFormatter.dateFormat = "yyyy"
         return dateFormatter.string(from: self)
     }
 }
